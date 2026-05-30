bce68c46bb47de772daefc2219b73fa222c82e4a
nicobuz2525 <nicolabuzzanca25@gmail.com>
2026-05-30 12:31:20 +0200

    Add files via upload

---
+++ Sito web-2/script.js
@@ -0,0 +1,25 @@
+let currentSlide = 0;
+const slides = document.querySelectorAll('.slide');
+
+function showSlide(index) {
+    slides.forEach((slide, i) => {
+        slide.style.display = i === index ? 'block' : 'none';
+    });
+}
+
+function nextSlide() {
+    currentSlide = (currentSlide + 1) % slides.length;
+    showSlide(currentSlide);
+}
+
+function prevSlide() {
+    currentSlide = (currentSlide - 1 + slides.length) % slides.length;
+    showSlide(currentSlide);
+}
+
+// Mostra la prima slide all'avvio
+showSlide(currentSlide);
+
+// Cambia slide automaticamente ogni 5 secondi
+setInterval(nextSlide, 5000);
+
